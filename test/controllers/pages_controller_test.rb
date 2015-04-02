13b825a6cfc03283ff13932e97600d3582d30ffa
Ben Wilz <bwilz@madisoncollege.edu>
2015-04-02 05:19:50 +0000

    most of a user model

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -6,9 +6,5 @@ class PagesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get user" do
-    get :user
-    assert_response :success
-  end
   
 end
