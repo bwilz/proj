44f22b0b11bc708a9aa9243a0c5dce76fb75425b
Ben Wilz <bwilz@madisoncollege.edu>
2015-04-16 23:23:50 +0000

    created admin signup

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+
+  config.force_ssl = true
 # Initialize the Rails application.
 Rails.application.initialize!
