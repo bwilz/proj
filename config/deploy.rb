faf327d3d3164e24097c27653cef2338d35be774
Ben Wilz <bwilz@madisoncollege.edu>
2015-05-14 02:03:16 +0000

    no idea

---
+++ config/deploy.rb
@@ -0,0 +1,31 @@
+# config valid only for Capistrano 3.1
+lock '3.2.1'
+
+set :application, 'proj'
+set :repo_url, 'git@github.com:bwilz/proj'
+set :rbenv_ruby, '2.2.2'
+
+# Default branch is :master
+ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+
+# Default deploy_to directory is /var/www/my_app
+set :deploy_to, '/var/www'
+
+# Default value for :pty is false
+set :pty, true
+
+# Default value for :linked_files is []
+set :linked_files, %w{config/database.yml}
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+       execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :publishing, :restart
+
+end
