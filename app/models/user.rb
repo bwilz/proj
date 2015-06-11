2d580f819a2e0bc0b54fdd2ad2621a05e1ca4577
Ben Wilz <bwilz@madisoncollege.edu>
2015-06-11 05:04:12 +0000

    nearly finished carousel

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ActiveRecord::Base
+  has_many :guest
   attr_accessor :remember_token, :activation_token
   before_save   :downcase_email
   before_create :create_activation_digest
   
   attr_accessor :remember_token, :activation_token, :reset_token
   
-  has_many :guest
+  
   validates :name, presence: true, length: {maximum:50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
